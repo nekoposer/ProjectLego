2770a080c2c028903f243a8ea47a613c08741a10
Zimanov Mikhail <148744599+nekoposer@users.noreply.github.com>
2023-11-23 17:47:47 +0100

    Delete src directory

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Start start = new Start();
-        start.setVisible(true);
-        start.pack();
-    }
-}
--- src/Main/Java/Main.java
+++
@@ -1,9 +0,0 @@
-import com.github.nekoposer.Start;
-
-public class Main {
-    public static void main(String[] args) {
-        Start start = new Start();
-        start.setVisible(true);
-        start.pack();
-    }
-}
